18b377f398656132d528ab276a8afe6126dd6b9f
Sawyer <sawyerjhood@gmail.com>
2014-07-23 16:19:32 -0400

    Added click handling to the cards.

--- core/src/com/sawyerhood/crosscard/screens/GameplayScreen.java
+++ core/src/com/sawyerhood/crosscard/screens/GameplayScreen.java
@@ -1,9 +1,15 @@
 package com.sawyerhood.crosscard.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.sawyerhood.crosscard.CrossCardGame;
 import com.sawyerhood.crosscard.actors.CardActor;
 import com.sawyerhood.crosscard.gamelogic.CrossCardGameManager;
@@ -16,6 +22,7 @@ public class GameplayScreen extends MenuScreen {
   private Table cardGameTable;
 
   public GameplayScreen(CrossCardGame game) {
+	  
     super(game);
     gameManager = new CrossCardGameManager();
     Texture cardImage = game.getAssetManger().get("card.png", Texture.class);
@@ -29,6 +36,16 @@ public class GameplayScreen extends MenuScreen {
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
         CardActor card = new CardActor(cardImage, font, null);
+        
+        card.addCaptureListener(new ClickListener() {
+
+            public void clicked(InputEvent event, float x, float y) {
+              
+              System.out.println(event.getListenerActor());
+
+            }
+          });
+        
         cardGameTable.add(card).pad(15);
         cardGrid[i][j] = card;
       }
