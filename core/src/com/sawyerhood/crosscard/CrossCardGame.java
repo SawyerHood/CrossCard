07c2d124b92c8b77f0c84fa04546ca06d0acc1f9
Matt Hansen <m.hansen@knights.ucf.edu>
2014-07-26 22:47:04 -0400

    Added sound resource for card draw

--- core/src/com/sawyerhood/crosscard/CrossCardGame.java
+++ core/src/com/sawyerhood/crosscard/CrossCardGame.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.SkinLoader;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -14,6 +15,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.sawyerhood.crosscard.screens.MainMenuScreen;
 
+/**
+ * The game class containing the initialization of resources and rendering method.
+ * 
+ * @Author Sawyer Hood
+ * @author Matt Hansen
+ * 
+ */
 public class CrossCardGame extends Game {
   protected SpriteBatch batch;
   protected Texture img;
@@ -25,9 +33,6 @@ public class CrossCardGame extends Game {
     batch = new SpriteBatch();
     // setScreen(new MainMenuScreen(this));
     initManager();
-
-
-
   }
 
   @Override
@@ -35,8 +40,9 @@ public class CrossCardGame extends Game {
     Gdx.gl.glClearColor(0, 0, 0, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     if (assetManager.update()) {
-      if (getScreen() == null)
+      if (getScreen() == null) {
         setScreen(new MainMenuScreen(this));
+      }
       if (music == null) {
         music = assetManager.get("music.mp3");
         music.play();
@@ -44,23 +50,19 @@ public class CrossCardGame extends Game {
       }
       super.render();
     }
-
-
-
   }
 
   @Override
   public void setScreen(Screen screen) {
-    if (getScreen() != null)
+    if (getScreen() != null) {
       getScreen().dispose(); // Get rid of old screen assets.
+    }
     super.setScreen(screen);
   }
 
-
-  public AssetManager getAssetManger() {
-    return assetManager;
-  }
-
+  /**
+   * Loads resources into the AssetManager.
+   */
   private void initManager() {
     assetManager = new AssetManager();
     assetManager.setLoader(Skin.class, new SkinLoader(new InternalFileHandleResolver()));
@@ -68,9 +70,23 @@ public class CrossCardGame extends Game {
     assetManager.load("default.fnt", BitmapFont.class);
     assetManager.load("card.png", Texture.class);
     assetManager.load("music.mp3", Music.class);
-
+    assetManager.load("card_draw.wav", Sound.class);
   }
 
+  /**
+   * Returns the asset manager.
+   * 
+   * @return the asset manager
+   */
+  public AssetManager getAssetManger() {
+    return assetManager;
+  }
+
+  /**
+   * Get the background music.
+   * 
+   * @return the background music
+   */
   public Music getMusic() {
     return music;
   }
